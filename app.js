744811f1467db440c427b04eb4c060cc8f00158b
Durmuş PALA <71263028+durmus-pala@users.noreply.github.com>
2021-04-08 12:50:16 +0300

    add Js codes

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
- function addToDoList(event) {
-    const myToDo = document.getElementById('todo').value;
-    let myUl = document.getElementById('ul');
-    let li = document.createElement('li');
-    let input1 = document.createElement('input');
-    input1.type = "checkbox";
-    let button1 = document.createElement('button');
-    button1.innerText = 'delete';
-    button1.style.width = '50px';
-    button1.style.height = '20px';
-    button1.style.color = 'red';
-    li.append(input1);
-    li.append(myToDo);
-    li.append(button1);
-    myUl.append(li);
- }
+/* eslint-disable linebreak-style */
+function addToDoList() {
+  const myToDo = document.getElementById('todo').value;
+  const myUl = document.getElementById('ul');
+  const li = document.createElement('li');
+  const input1 = document.createElement('input');
+  input1.type = 'checkbox';
+  const button1 = document.createElement('button');
+  button1.innerText = 'delete';
+  button1.style.width = '50px';
+  button1.style.height = '20px';
+  button1.style.color = 'red';
+  button1.addEventListener('click', () => {
+    myUl.removeChild(li);
+  });
+  li.append(input1);
+  li.append(myToDo);
+  li.append(button1);
+  myUl.append(li);
+}
 
 const addButton = document.getElementById('addItem');
 const toDo = document.getElementById('todo');
 
 addButton.addEventListener('click', addToDoList);
-toDo.addEventListener('keypress', function(e){
-   if(e.key === 'Enter'){
-      addToDoList();
-   }
+toDo.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    addToDoList();
+  }
 });
-
-
-
-
-
